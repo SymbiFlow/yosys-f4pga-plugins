c72a4fccba376289f715f190c8777aace7910272
Tarachand Pagarani <tpagarani@qlsof02.quicklogic.com>
2023-05-08 09:35:11 -0700

    qlf_k6n10f/cells_sim.v

--- ql-qlf-plugin/synth_quicklogic.cc
+++ ql-qlf-plugin/synth_quicklogic.cc
@@ -227,7 +227,7 @@ struct SynthQuickLogicPass : public ScriptPass {
 
         if (abc9 && design->scratchpad_get_int("abc9.D", 0) == 0) {
             log_warning("delay target has not been set via SDC or scratchpad; assuming 12 MHz clock.\n");
-            design->scratchpad_set_int("abc9.D", 41667); // 12MHz = 83.33.. ns; divided by two to allow for interconnect delay.
+            design->scratchpad_set_int("abc9.D", 500); // 12MHz = 83.33.. ns; divided by two to allow for interconnect delay.
         }
 
         log_header(design, "Executing SYNTH_QUICKLOGIC pass.\n");
